a5aab8c04b18fef762015684a49ea8e3d6daebef
Alexander Bokovoy <abokovoy@redhat.com>
2019-10-17 21:56:17 +0300

    GC: add Global Catalog instance installer
    Add installer/uninstaller for Global Catalog
    
    In order to install:
      /usr/libexec/ipa/gc/ipa-gc-install --gc-password 'pwd' -U
    - installs the global catalog as a dirsrv instance in
      /etc/dirsrv/slapd-GLOBAL-CATALOG
    - the instance has a cn=Directory Manager user with 'pwd'
    - the instance is listening on ports 3268 and 3269
    - for CA-less installs, specify --gc-cert-file 'pkcs12' --gc-pin 'pin'
    
    In order to uninstall:
      /usr/libexec/ipa/ipa-gc-install --uninstall -U
    - removes the instance
    
    The installation creates an entry
    cn=GLOBAL-CATALOG,cn=$hostname,cn=masters,cn=ipa,cn=etc,$BASEDN
    which means that ipactl start/ipactl stop also starts/stops the GC.
    
    Signed-off-by: Alexander Bokovoy <abokovoy@redhat.com>
    Signed-off-by: Florence Blanc-Renaud <flo@redhat.com>
    Fixes: https://pagure.io/freeipa/issue/3125

--- ipaserver/install/server/install.py
+++ ipaserver/install/server/install.py
@@ -39,7 +39,7 @@ from ipalib.util import (
 )
 from ipalib.facts import IPA_MODULES
 from ipaserver.install import (
-    adtrust, adtrustinstance, bindinstance, ca, dns, dsinstance,
+    adtrust, adtrustinstance, bindinstance, ca, dns, dsinstance, gc,
     httpinstance, installutils, kra, krbinstance,
     otpdinstance, custodiainstance, replication, service,
     sysupgrade, cainstance)
@@ -1119,6 +1119,7 @@ def uninstall_check(installer):
                 "procedure?", False)):
             raise ScriptError("Aborting uninstall operation.")
     else:
+        gc.uninstall_check()
         dns.uninstall_check(options)
 
         ca.uninstall_check(options)
@@ -1170,6 +1171,9 @@ def uninstall(installer):
     # Uninstall the KRA prior to shutting the services down so it
     # can un-register with the CA.
     kra.uninstall()
+
+    print("Removing Global Catalog")
+    gc.uninstall(fstore)
 
     print("Shutting down all IPA services")
     try:
