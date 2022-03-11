896d22ce187b4346c736721c995625e0854d8b36
Francisco Trivino <ftrivino@redhat.com>
2022-03-11 17:47:38 +0100

    Set AES as default for KRA archival wrapping
    This commit sets AES-128-CBC as default wrapping algorithm as
    TripleDES (des-ede3-cbc) is not supported anymore in C9S.
    
    Fixes: https://pagure.io/freeipa/issue/6524
    
    Signed-off-by: Francisco Trivino <ftrivino@redhat.com>
    Reviewed-By: Alexander Bokovoy <abokovoy@redhat.com>
    Reviewed-By: Christian Heimes <cheimes@redhat.com>

--- ipalib/constants.py
+++ ipalib/constants.py
@@ -26,6 +26,8 @@ import os
 import socket
 from ipapython.dn import DN
 from ipapython.version import VERSION, API_VERSION
+from cryptography.hazmat.primitives.ciphers import algorithms, modes
+from cryptography.hazmat.backends.openssl.backend import backend
 
 try:
     FQDN = socket.getfqdn()
@@ -345,10 +347,12 @@ SOFTHSM_DNSSEC_TOKEN_LABEL = u'ipaDNSSEC'
 VAULT_WRAPPING_3DES = 'des-ede3-cbc'
 VAULT_WRAPPING_AES128_CBC = 'aes-128-cbc'
 VAULT_WRAPPING_SUPPORTED_ALGOS = (
-    # old default was 3DES
-    VAULT_WRAPPING_3DES,
-    # supported since pki-kra >= 10.4
+    # new default and supported since pki-kra >= 10.4
     VAULT_WRAPPING_AES128_CBC,
 )
-# 3DES for backwards compatibility
-VAULT_WRAPPING_DEFAULT_ALGO = VAULT_WRAPPING_3DES
+VAULT_WRAPPING_DEFAULT_ALGO = VAULT_WRAPPING_AES128_CBC
+
+# Add 3DES for backwards compatibility if supported
+if backend.cipher_supported(algorithms.TripleDES(b"\x00" * 8),
+                            modes.CBC(b"\x00" * 8)):
+    VAULT_WRAPPING_SUPPORTED_ALGOS += (VAULT_WRAPPING_3DES,)
