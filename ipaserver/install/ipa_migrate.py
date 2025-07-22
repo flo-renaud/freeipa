94bea7c13fe5a7748d389c6bd96944a96d654d1c
Florence Blanc-Renaud <flo@redhat.com>
2025-07-22 18:07:11 +0200

    Revert "ipa-migrate - improve suffix replacement"
    This reverts commit 6cdabdacc950e4c334eb4a3e1666b19178072e36.

--- ipaserver/install/ipa_migrate.py
+++ ipaserver/install/ipa_migrate.py
@@ -1083,9 +1083,11 @@ class IPAMigrate():
         if isinstance(val, bytes) or isinstance(val, DN):
             return val
 
+        # Replace base DN
+        val = self.replace_suffix_value(val)
+
         # For DNS DN we only replace suffix
         if dns:
-            val = self.replace_suffix_value(val)
             return val
 
         # Replace host
@@ -1098,9 +1100,6 @@ class IPAMigrate():
 
         # Replace realm
         val = val.replace(self.remote_realm, self.realm)
-
-        # Lastly, replace base DN
-        val = self.replace_suffix_value(val)
 
         return val
 
